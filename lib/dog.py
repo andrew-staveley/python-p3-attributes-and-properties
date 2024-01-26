71483ed57f1f244e3e8f06aa50254b9a3ddfcd1e
Andrew <andrewstaveley14@gmail.com>
2024-01-26 18:53:04 -0500

    Completed Assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,6 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Dog Name Here", breed="Mastiff"):
+        self.name = name if (type(name) is (str)) and (1 <= len(name) <= 25) else print("Name must be string between 1 and 25 characters.") 
+        self.breed = breed if (breed in APPROVED_BREEDS) else print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,6 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Person Name Here", job="Admin"):
+        self.name = name.title() if (type(name) is (str)) and (1 <= len(name) <= 25) else print("Name must be string between 1 and 25 characters.")
+        self.job = job if (job in APPROVED_JOBS) else print("Job must be in list of approved jobs.")
